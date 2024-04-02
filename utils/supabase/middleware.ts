1fae476adfd99e54c6cf3376770d5a9ce1128761
dumvan <felizavndrd567@gmail.com>
2024-04-02 14:24:58 +0800

    chore: setup middleware

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -2,9 +2,7 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export const updateSession = async (request: NextRequest) => {
-  // This `try/catch` block is only here for the interactive tutorial.
-  // Feel free to remove once you have Supabase connected.
-  try {
+    try {
     // Create an unmodified response
     let response = NextResponse.next({
       request: {
